0473b33059dd443954a67183e9deb2aac55c3ad3
Anna Lartey <anna.t.lartey@gmail.com>
2022-07-08 12:19:21 +0000

    worked on challenge 1 and 2

--- src/assignment.js
+++ src/assignment.js
@@ -13,10 +13,20 @@ const assignment = {};
  * @param {Array} arrayOfNumbers the array of numbers to sum
  * @returns number the sum of the numbers
  */
+let arrayOfNumbers = [2, 3, 4, 5, 6,10]
+console.log("This is my array of numbers: " + arrayOfNumbers);
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    // console.log (arrayOfNumbers.length)
+    // console.log (arrayOfNumbers[0] + arrayOfNumbers[1] + arrayOfNumbers[2])
+  let sum = 0;
+  for( i=0; i < arrayOfNumbers.length; i++){
+    sum += arrayOfNumbers[i]
+  }
+  console.log ("The sum of the numbers in this array is: " + sum)
 }
-// assignment.sumOfNumbers = sumOfNumbers;
+sumOfNumbers(arrayOfNumbers)
+
 
 /**
  * Challenge - 2
@@ -27,10 +37,25 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
+
+
 function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+    let evenNumbers = arrayOfNumbers.filter((n) => n % 2 == 0);
+  console.log("These are the even numbers in my array of numbers: " + evenNumbers)
+  console.log("The number of even numbers in my array of numbers is: " + evenNumbers.length)
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+
+countEvenNumbers(arrayOfNumbers)
+
+
+function countOddNumbers(arrayOfNumbers) {
+    let oddNumbers = arrayOfNumbers.filter((n) => n % 2 == 1);
+  console.log("These are the odd numbers in my array of numbers: " + oddNumbers)
+  console.log("The number of odd numbers in my array of numbers is: " + oddNumbers.length)
+}
+
+countOddNumbers(arrayOfNumbers)
+
 
 /**
  * Challenge - 3
